34e77039822ffbee39f21b6fcdab297aba65dfea
maav6848 <maav6848@gmail.com>
2022-03-24 18:22:08 +0300

    2

--- djsite/mydjangosite/apartments/views.py
+++ djsite/mydjangosite/apartments/views.py
@@ -2,6 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<H1> Главная страница приложения <H1/>")
+    return HttpResponse("<H1> Главная страница приложения </H1>")
 
+def catigories(request):
+    return HttpResponse("Категории")
 
+#def rent(response):
+#    return HttpResponse("Аренда квартир")
+
+#def delivery(response):
+ #   return HttpResponse("Сдача квартир в аренду")
--- djsite/mydjangosite/mydjangosite/urls.py
+++ djsite/mydjangosite/mydjangosite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from apartments.views import index
+from apartments.views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apartments/', index),
+    path('apartment/', include('apartment.urls')),
+
+
 ]
